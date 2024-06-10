72065eb6866485e8eb4806ed29ad631d5e095674
CaiHeran <Tsai.love.dev@outlook.com>
2024-06-10 17:58:33 +0800

    添加项目文件。

---
+++ server/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Net;
+
+namespace Server
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            /*if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: server <port>\nPress any key to exit.");
+                Console.ReadKey();
+                return;
+            }*/
+            int port = args.Length > 0 ? int.Parse(args[0]) : 23456;
+            Console.WriteLine("This server is running on port {0}", port);
+            Console.WriteLine("Server's IPs:");
+            ShowIPs();
+        }
+        static void ShowIPs()
+        {
+            IPAddress[] IPs = Dns.GetHostAddresses(Dns.GetHostName());
+            foreach (IPAddress IP in IPs)
+                Console.WriteLine(IP.ToString());
+        }
+    }
+}
